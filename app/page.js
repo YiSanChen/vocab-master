6ea790defb8e0d984347aebc682de724e997f148
YiSanChen <abc42656597@gmail.com>
2025-12-03 02:57:34 +0800

    Update v2

--- app/page.js
+++ app/page.js
@@ -154,10 +154,10 @@ export default function Home() {
           <Brain className="text-white w-10 h-10" />
         </div>
         <h1 className="text-4xl font-extrabold mb-3 text-gray-900 tracking-tight">Vocab Master</h1>
-        <p className="text-gray-500 mb-10 text-lg">打造你的專屬英文單字庫，<br/>用科學方法記憶單字。</p>
+        <p className="text-gray-500 mb-10 text-lg">要不是曾國城在古蹟抽菸，<br/>我多益就拿金證了</p>
         <button onClick={handleLogin} className="w-full py-4 bg-gray-900 hover:bg-gray-800 text-white rounded-xl font-bold text-lg transition-all shadow-xl hover:shadow-2xl flex items-center justify-center gap-3">
           <svg className="w-6 h-6" viewBox="0 0 24 24"><path fill="currentColor" d="M12.545,10.239v3.821h5.445c-0.712,2.315-2.647,3.972-5.445,3.972c-3.332,0-6.033-2.701-6.033-6.032s2.701-6.032,6.033-6.032c1.498,0,2.866,0.549,3.921,1.453l2.814-2.814C17.503,2.988,15.139,2,12.545,2C7.021,2,2.543,6.477,2.543,12s4.478,10,10.002,10c8.396,0,10.249-7.85,9.426-11.748L12.545,10.239z"/></svg>
-          使用 Google 登入
+          使用 🚬曾國城的菸頭 登入
         </button>
       </div>
     </div>
